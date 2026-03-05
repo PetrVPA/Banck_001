e5f5cfc78fba4cd66298898f90542c0d774f7095
PetrVPA <vpa.interceptor@gmail.com>
2026-03-05 16:02:08 +0300

    del error flake8

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,3 @@
-from operator import truediv
-
 
 def get_mask_card_number(number_card: int) -> str:
     """ Принимает номер карты 16 чисел и выдает замаскированый ответ: 1234 12** **** 1234
@@ -11,20 +9,22 @@ def get_mask_card_number(number_card: int) -> str:
     oktet_1...4 str  переменные октеты для формирования строки ответа функции
     oktet_2 и oktet_3 в полном виде закоментированы на случай оперативного изменения маски
     """
-    mask_card:str = ""
+    mask_card: str = ""
     index_card = str(number_card)
     lenght_digit_card = len(index_card)
     check_digit = index_card.isdigit()
-    if lenght_digit_card == 16 and check_digit == True:
+    if lenght_digit_card == 16 and check_digit:
         oktet_1 = index_card[0:4]
-        oktet_2 = index_card[4:6] + "**" # oktet_2 = index_card[4:8]
-        oktet_3 = "****" # oktet_3 = index_card[8:12]
+        oktet_2 = index_card[4:6] + "**"
+        # oktet_2 = index_card[4:8]
+        oktet_3 = "****"
+        # oktet_3 = index_card[8:12]
         oktet_4 = index_card[12:16]
         mask_card = oktet_1 + " " + oktet_2 + " " + oktet_3 + " " + oktet_4
     else:
-        if lenght_digit_card != 16 and check_digit == True:
+        if lenght_digit_card != 16 and check_digit:
             print("Неверное колличество цифр")
-        if check_digit != True:
+        if not check_digit:
             print("Введены символы отличные от цифры")
     return str(mask_card)
 
@@ -37,15 +37,15 @@ def get_mask_account(number_account: int) -> str:
     index_account int   переменная контролирующая количество вводимых символов
     check_digit bool  переменная контролирующая что пользователь ввел = только цифры
     """
-    mask_account:str = ""
+    mask_account: str = ""
     index_account = str(number_account)
-    index_account = len(index_account)
+    lenght_digit_account = len(index_account)
     check_digit = index_account.isdigit()
-    if lenght_digit_account == 20 and check_digit == True:
+    if lenght_digit_account == 20 and check_digit:
         mask_account = "**" + index_account[16:21]
     else:
-        if lenght_digit_account != 20 and check_digit == True:
+        if lenght_digit_account != 20 and check_digit:
             print("Неверное колличество цифр")
-        if check_digit != True:
+        if not check_digit:
             print("Введены символы отличные от цифры")
     return str(mask_account)
