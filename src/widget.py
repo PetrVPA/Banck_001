873227bb55ea86239445f9a1d81e9661f6f3b835
PetrVPA <vpa.interceptor@gmail.com>
2026-03-05 16:54:50 +0300

    edit dogstring

--- src/widget.py
+++ src/widget.py
@@ -7,7 +7,9 @@ def mask_account_card(card_data: str) -> str:
     Функция принимает зачение кары или счета в виде Visa Platinum 7000792289606361 или Счет 73654108430135874305
     возвращает маскированные данные карты или счета в виде Visa Platinum 7000 79** **** 6361 для карты и Счет **4305
     для счета.
-
+    card_data переменная принимающая значение введенное пользователем
+    card_set переменная контроля алгоритма работы с картой
+    account_set преременная контроля алгоритма работы со счетом
 
     '''
     card_set = card_data[-16:]
@@ -16,12 +18,14 @@ def mask_account_card(card_data: str) -> str:
     change_card = card_set.isdigit()
 
     if change_account:
-        result_digit_account = get_mask_account(account_set)
-        res_fool = "Счет "+result_digit_account
+        account = int(account_set)
+        result_digit_account = get_mask_account(account)
+        res_fool = "Счет " + result_digit_account
         return res_fool
 
     if change_card:
-        result_digit_card = get_mask_card_number(card_set)
+        card = int(card_set)
+        result_digit_card = get_mask_card_number(card)
         res_fool = card_data[:-16] + result_digit_card
         return res_fool
     return "Ошибка ввода"
