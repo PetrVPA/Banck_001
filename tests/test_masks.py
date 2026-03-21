988b1142c2fb89f43235481ca41e1e5d1d437b82
PetrVPA <vpa.interceptor@gmail.com>
2026-03-21 10:42:58 +0300

    edit PEP8 parameterization tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,26 +1,23 @@
+import pytest
 from src.widget import get_mask_account
 from src.masks import get_mask_card_number
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number("1234123412341234") == "1234 12** **** 1234"
+@pytest.mark.parametrize("input_set, output_set", [
+    ("1234123412341234", "1234 12** **** 1234"),
+    ("12341234123412344", ""),
+    ("123412341234123t", ""),
+    ("123412342341234", "")
+])
+def test_get_mask_card_number(input_set, output_set):
+    assert get_mask_card_number(input_set) == output_set
 
 
-def test_get_mask_card_number_err1():
-    assert get_mask_card_number("12341234123412344") == ""
-
-
-def test_get_mask_card_number_err2():
-    assert get_mask_card_number("123412341234123t") == ""
-
-
-def test_get_mask_account():
-    assert get_mask_account("12345678901234567890") == "**7890"
-
-
-def test_get_mask_account_err1():
-    assert get_mask_account("123456789012p34567890") == ""
-
-
-def test_get_mask_account_err2():
-    assert get_mask_account("123456789012534567890") == ""
+@pytest.mark.parametrize("input_set, output_set", [
+    ("12345678901234567890", "**7890"),
+    ("123456789012p3456790", ""),
+    ("123456789012534567890", ""),
+    ("1234567843901253790", "")
+])
+def test_get_mask_account(input_set, output_set):
+    assert get_mask_account(input_set) == output_set
