301389b1a106c6d781842fc309617c8b335adc11
PetrVPA <vpa.interceptor@gmail.com>
2026-04-02 14:15:52 +0300

    next saving work (card_numer_generator)

--- src/generators.py
+++ src/generators.py
@@ -1,11 +1,12 @@
 
 
-def filter_by_currency():
+def filter_by_currency(list_operation, key_filter):
     '''
     новая функция
     :return:
     '''
-    pass
+    result = [tile for tile in list_operation if tile ["operationAmount"] ["currency"] ["code"] == key_filter]
+    return result
 
 
 def transaction_descriptions():
@@ -18,8 +19,11 @@ def transaction_descriptions():
 
 def card_number_generator(start, finish):
     '''
-    генератор
-    :return:
+    генератор формирующий карты в формате октетов с пробелами, начиная от стартового до финишного значений
+    range_card формирует карты к шестнадцати разрядной форме номера карты
+    list_octet формирует список октетов содержащихся в номере карты
+    one_card сформированный номер карты в нужном формате (готовый)
+    return output_card список уже готовых карт с номерами от стартового до финишного
     '''
     output_card: list = []
     range_card = [str(x).zfill(16) for x in range(start, finish + 1)]
@@ -28,4 +32,3 @@ def card_number_generator(start, finish):
         one_card = " ".join(list_octet)
         output_card.append(one_card)
     return output_card
-
