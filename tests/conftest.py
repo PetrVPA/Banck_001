69e0d08a52205cfd2bcafc50ce5f82eeb6eb8e22
PetrVPA <vpa.interceptor@gmail.com>
2026-04-04 18:59:14 +0300

    finish edit

--- tests/conftest.py
+++ tests/conftest.py
@@ -119,7 +119,23 @@ def question_transactions():
 
 @pytest.fixture
 def question_transactions1():
-    return [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "operationAmount": {"amount": "9824.07","currency": {"name": "USD","code": "USD"}},"description": "Перевод организации","from": "Счет 75106830613657916952","to": "Счет 11776614605963066702"}]
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        }
+    ]
 
 
 @pytest.fixture
@@ -284,7 +300,7 @@ def answer_transactions4():
 
 @pytest.fixture
 def answer_transactions5():
-    return  {
+    return {
             "id": 594226727,
             "state": "CANCELED",
             "date": "2018-09-12T21:27:25.241689",
