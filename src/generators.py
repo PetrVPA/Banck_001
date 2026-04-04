448a71b0dfc047b72bfa70c7208fec71260a6c10
PetrVPA <vpa.interceptor@gmail.com>
2026-04-04 17:53:40 +0300

    edit tests

--- src/generators.py
+++ src/generators.py
@@ -9,9 +9,8 @@ def filter_by_currency(list_operation: list, key_filter: str) -> list:
     list_operation перечень (список) всех анализируемых транзакций
     key_filter ключ определяющий выбор валюты для обработки транзакций
     '''
-    completed_transactions = (
-        meaning_key for meaning_key in list_operation if meaning_key["operationAmount"]["currency"][
-            "code"] == key_filter)
+    completed_transactions = (meaning_key for meaning_key in list_operation if meaning_key["operationAmount"][
+        "currency"]["code"] == key_filter)
     return completed_transactions
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -119,23 +119,7 @@ def question_transactions():
 
 @pytest.fixture
 def question_transactions1():
-    return [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        }
-    ]
+    return [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "operationAmount": {"amount": "9824.07","currency": {"name": "USD","code": "USD"}},"description": "Перевод организации","from": "Счет 75106830613657916952","to": "Счет 11776614605963066702"}]
 
 
 @pytest.fixture
@@ -244,19 +228,19 @@ def answer_transactions1():
 @pytest.fixture
 def answer_transactions2():
     return {
-            "id": 873106923,
+            "id": 142264268,
             "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
+            "date": "2019-04-04T23:20:05.206878",
             "operationAmount": {
-                "amount": "43318.34",
+                "amount": "79114.93",
                 "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
+                    "name": "USD",
+                    "code": "USD"
                 }
             },
             "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
     }
 
 
@@ -300,7 +284,7 @@ def answer_transactions4():
 
 @pytest.fixture
 def answer_transactions5():
-    return {
+    return  {
             "id": 594226727,
             "state": "CANCELED",
             "date": "2018-09-12T21:27:25.241689",
@@ -318,24 +302,9 @@ def answer_transactions5():
 
 
 @pytest.fixture
-def question_transactions6():
-    return []
-
-
-@pytest.fixture
-def answer_transactions6():
-    return []
-
-
-@pytest.fixture
 def answer_descriptions1():
     return ['Перевод организации', 'Перевод со счета на счет',
             'Перевод со счета на счет', 'Перевод с карты на карту', 'Перевод организации']
-
-
-@pytest.fixture
-def answer_descriptions2():
-    return []
 
 
 @pytest.fixture
