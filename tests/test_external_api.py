a6cccbc0bc45902949b4ab8cd5e8bbc78f0cce83
PetrVPA <vpa.interceptor@gmail.com>
2026-04-30 10:25:33 +0300

    Rough draft homework

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -16,6 +16,36 @@ trans_action1 = {
     'to': 'Счет 64686473678894779589'
 }
 
+trans_action2 = {
+    'id': 441945116,
+    'state': 'EXECUTED',
+    'date': '2019-08-26T10:50:58.294041',
+    'operationAmount': {'amount': '46012.31', 'currency': {'name': 'USD.', 'code': 'USD'}},
+    'description': 'Перевод организации',
+    'from': 'Maestro 1596837868705199',
+    'to': 'Счет 64686473678894779589'
+}
+
+trans_action3 = {
+    'id': 441945776,
+    'state': 'EXECUTED',
+    'date': '2019-08-26T10:50:58.294041',
+    'operationAmount': {'amount': '28836.74', 'currency': {'name': 'RUB.', 'code': 'RUB'}},
+    'description': 'Перевод организации',
+    'from': 'Maestro 1596837868705199',
+    'to': 'Счет 64686473678894779589'
+}
+
+trans_action4 = {
+    'id': 441945336,
+    'state': 'EXECUTED',
+    'date': '2019-08-26T10:50:58.294041',
+    'operationAmount': {'amount': '123867.54', 'currency': {'name': 'CNH.', 'code': 'CNH'}},
+    'description': 'Перевод организации',
+    'from': 'Maestro 1596837868705199',
+    'to': 'Счет 64686473678894779589'
+}
+
 
 @patch('requests.request')
 def test_amout_rur1(mock_request):
@@ -37,3 +67,48 @@ def test_amout_rur1(mock_request):
         headers={"apikey": api_token},
         data={}
     )
+
+@patch('requests.request')
+def test_amout_rur2(mock_request):
+    # Настраиваем заглушку — мокаем Response.text
+    mock_response = mock_request.return_value
+    mock_response.text = '{"result": 46012.31}'
+
+    # Вызываем тестируемую функцию
+    result = amout_rur(trans_action2)
+
+    # Проверяем результат
+    assert result == 46012.31
+
+    # Проверяем, что запрос был сделан с правильными параметрами
+    expected_url = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=46012.31"
+    mock_request.assert_called_once_with(
+        "GET",
+        expected_url,
+        headers={"apikey": api_token},
+        data={}
+    )
+
+@patch('requests.request')
+def test_amout_rur3(mock_request):
+    # Настраиваем заглушку — мокаем Response.text
+    mock_response = mock_request.return_value
+    mock_response.text = '{"result": 28836.74}'
+
+    # Вызываем тестируемую функцию
+    result = amout_rur(trans_action3)
+
+    # Проверяем результат
+    assert result == 28836.74
+
+@patch('requests.request')
+def test_amout_rur4(mock_request):
+    # Настраиваем заглушку — мокаем Response.text
+    mock_response = mock_request.return_value
+    mock_response.text = '{"result": 0}'
+
+    # Вызываем тестируемую функцию
+    result = amout_rur(trans_action4)
+
+    # Проверяем результат
+    assert result == ()
